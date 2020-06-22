b603dedc2e6840fbc57f260ccb52434b6bd79264
Franco Queirolo <fgqueirolo@gmail.com>
2020-06-22 16:46:04 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './services/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModu
